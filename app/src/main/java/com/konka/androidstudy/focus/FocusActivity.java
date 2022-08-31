80e7ba15c49570f290b2b372e5a3b227d5cf2d7f
zhenli2017 <1179581524@qq.com>
2022-08-31 12:52:24 +0800

    tiaozhuan

--- app/src/main/java/com/konka/androidstudy/focus/FocusActivity.java
+++ app/src/main/java/com/konka/androidstudy/focus/FocusActivity.java
@@ -1,12 +1,17 @@
 package com.konka.androidstudy.focus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Parcel;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.konka.androidstudy.R;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.LinkedHashMap;
 
 public class FocusActivity extends AppCompatActivity {
 
@@ -14,13 +19,21 @@ public class FocusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_focus);
-        byte[] bytes = new byte[1024*1024*10];
+        byte[] bytes = new byte[1024 * 1024 * 10];
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.gc();
             }
         });
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<String, String>(0,0.75f,true);
+        linkedHashMap.put("1","1");
+        linkedHashMap.put("2","2");
+        System.out.println("linkedHashMap = "+linkedHashMap);
+        ReferenceQueue<String> referenceQueue = new ReferenceQueue<>();
+        WeakReference<String> weakReference = new WeakReference<>("AAAA",referenceQueue);
+
+        Parcel.obtain();
     }
 
     @Override
